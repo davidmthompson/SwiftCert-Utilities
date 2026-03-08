e7a89f14beacbf3dfe8e33ab46bef7426e6a058c
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2026-03-08 13:48:09 -0600

    Initial Commit

---
+++ SwiftCert-Utilities/SwiftCert_UtilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCert_UtilitiesApp.swift
+//  SwiftCert-Utilities
+//
+//  Created by Morgan Pritchard on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCert_UtilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
