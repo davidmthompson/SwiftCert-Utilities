0626ede799b051127d6848206956a64faa55ea58
Stryker <stryker.shaw@fhnw.edu>
2026-03-09 10:33:06 -0600

    Added Eval bar

---
+++ SwiftCert-Utilities/PasswordStrength/EvalBar.swift
@@ -0,0 +1,18 @@
+//
+//  EvalBar.swift
+//  SwiftCert-Utilities
+//
+//  Created by Student on 3/9/26.
+//
+
+import SwiftUI
+
+struct EvalBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvalBar()
+}
