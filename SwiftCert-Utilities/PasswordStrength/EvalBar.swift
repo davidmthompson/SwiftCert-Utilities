9553d9f1e5c3235815a5b4e91d8439174dc5d82d
Stryker <stryker.shaw@fhnw.edu>
2026-03-09 10:34:06 -0600

    Addded eval bar

--- SwiftCert-Utilities/PasswordStrength/EvalBar.swift
+++ SwiftCert-Utilities/PasswordStrength/EvalBar.swift
@@ -7,10 +7,58 @@
 
 import SwiftUI
 
+
+
 struct EvalBar: View {
+  
+  @State private var password: String = ""
+  @State private var progress: Double = 0.1
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+          
+        Text("Password")
+          
+        TextField("Password", text: $password)
+          
+        ProgressView(value: progress)
+            .progressViewStyle(.linear)
+            .tint(progressColor)
+            .padding()
+          
+          Text(progressText)
+            .font(.headline)
+          
+          Button("Increase Progress") {
+            progress += 0.1
+          }
+      }
     }
+  
+  var progressColor: Color {
+    if progress > 1 {
+      return .green
+    } else if progress < 0.3 {
+      return .red
+    } else if progress < 0.3{
+      return .orange
+    } else {
+      return .yellow
+    }
+  }
+  
+  var progressText: String {
+    if progress > 1 {
+      return "Excellent"
+    } else if progress < 0.3 {
+      return "Bad"
+    } else if progress < 0.5{
+      return "Okay"
+    } else {
+      return "Strong"
+    }
+  }
+  
 }
 
 #Preview {
